ddea9f6a083da4d9d501850e663e920377029083
vtorre90 <87823112+vtorre90@users.noreply.github.com>
2021-09-22 12:00:30 +0200

    Update room area.py

--- room area.py
+++ room area.py
@@ -1,8 +1,12 @@
+#area calculation from user inputs 
+
 print ("STARTING PROGRAMME")
 print ("------------------")
 width = input("Write the width:")
 lenght = input ("Write the lenght:")
 
+//casting 
 area = float(width) * float(lenght)
 
+//usage of format() method
 print("The area of {0} and {1} is {2} mtq".format(width, lenght, area))
