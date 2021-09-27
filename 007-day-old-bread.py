e0886601a1cfbe68ab8b78b0c56ebbd843cf1ccb
vtorre90 <87823112+vtorre90@users.noreply.github.com>
2021-09-27 15:20:28 +0200

    Add files via upload

---
+++ 007-day-old-bread.py
@@ -0,0 +1,29 @@
+# A bakery sells loaves of bread for €3.49 each. Day old bread is discounted by 60 percent. 
+# Write a program that begins by reading the number of loaves of day old bread being purchased 
+# from the user. Then your program should display the regular price for the bread, 
+# the discount because it is a day old, and the total price. Each of these amounts should be 
+# displayed on its own line with an appropriate label. All of the values should be displayed using 
+# two decimal places, and the decimal points in all of the numbers should be aligned when reasonable 
+# values are entered by the user.
+
+print ('Write the number of loaves you want to get')
+
+input_loaves = int (input())
+
+price_one_loaves = 0.30
+print ('Number of choosen loaves:' + str(input_loaves))
+print ('The price for one loave is: ' + str(price_one_loaves)+'$')
+
+sum_loaves = price_one_loaves * input_loaves
+
+discount_value = sum_loaves / 100 * 60
+
+sum_loaves_discounted = sum_loaves - discount_value
+
+print ('The total discounted price is: ' + str(round(sum_loaves_discounted, 3)) +'$')
+
+
+
+
+
+
