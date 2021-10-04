77a6ac363a37013ccb54ae44d0ddd66adc0f4e65
vtorre90 <87823112+vtorre90@users.noreply.github.com>
2021-10-04 17:21:02 +0200

    Add files via upload

---
+++ 020-discount-table-2.py
@@ -0,0 +1,13 @@
+
+inp = -1 
+sum_of_digits = 0
+while (inp !=0):
+  num = input ('Type a num ')
+  if num == 0: 
+    break
+  elif num != 0:
+    sum_of_digits += int(num)
+    print (sum_of_digits)
+    
+  
+
